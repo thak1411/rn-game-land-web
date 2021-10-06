6b9630f60728eb9d3cb9c7a420e90a5ffeca199f
thak1411 <thak1411@gmail.com>
2021-10-07 03:22:46 +0900

    Add: Web Socket Testing Code

--- src/views/header/header.vue
+++ src/views/header/header.vue
@@ -1,16 +1,24 @@
 <template lang="pug">
 header.rn-header
-    |header
+    rntxt(init_message="Rn Game Land" :init_fontSize="22")
 </template>
 
 <script>
+import rntxt from '../components/rntxt.vue';
+
 export default {
-    name: "rn-header",
+    name: 'rn-header',
+    components: {
+        rntxt,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .rn-header {
     width: 100%;
+    height: 50px;
+    line-height: 50px;
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 6px 0px;
 }
 </style>
--- src/views/section/index/section.vue
+++ src/views/section/index/section.vue
@@ -1,11 +1,48 @@
 <template lang="pug">
 div.index-section
-    | section
+    input#rn-ws(type="text")
+    button(@click="onClick") SEND
 </template>
 
 <script>
+import { onMounted, ref } from 'vue';
+
 export default {
     name: 'index-section',
+    setup: function(props) {
+        let ws = {};
+        onMounted(() => {
+            const scheme = window.location.protocol == 'https:' ? 'wss://' : 'ws://';
+            const uri = scheme + window.location.hostname + ':8192/ws/test';
+
+            ws = new WebSocket(uri);
+            ws.onopen = function() {
+                console.log('Connected');
+            };
+            ws.onclose = function() {
+                console.log('Closed');
+            }
+            ws.onmessage = function(e) {
+                console.log('RCV', e.data);
+                console.log();
+                console.log(e);
+            }
+            ws.onerror = function(e) {
+                console.log('err', e)
+            }
+        });
+        const onClick = function() {
+            const data = document.querySelector('#rn-ws').value
+            console.log('send', data);
+            ws.send(data);
+            document.querySelector('#rn-ws').value = '';
+        }
+
+        return {
+            ws,
+            onClick,
+        };
+    }
 }
 </script>
 
